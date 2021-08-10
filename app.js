97d1f79b4dc6fd14f95ce592c74af0cd57625cd5
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2021-08-10 17:53:56 +0530

    Add environment variables

---
+++ app.js
@@ -0,0 +1,80 @@
+require("dotenv").config()
+const express = require("express")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const mongoose_encrypt = require("mongoose-encryption") 
+const app = express()
+
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true})
+
+const userSchema = new mongoose.Schema( {
+    email:{
+        type: String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+})
+
+const secret = "This is our little secret"
+userSchema.plugin(mongoose_encrypt, {secret: process.env.API_KEY, encryptedFields: ["password"]})
+
+
+const User = new mongoose.model("User",userSchema)
+
+app.get("/",function(req, res){
+    res.render("home")
+})
+
+app.get("/login",function(req, res){
+    res.render("login")
+}) 
+
+app.get("/register",function(req,res){
+    res.render("register")
+})
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        } else{
+            res.render("secrets")
+        }
+    })
+})
+
+app.post("/login",function(req,res){
+    const userEmail = req.body.username
+    const userPassword = req.body.password
+    User.findOne({email:userEmail},function(err,foundUser){
+        if(err)
+        {
+            console.log(err)
+        } else {
+            if(foundUser){
+                if(foundUser.password===userPassword){
+                    console.log(foundUser.email)
+                    console.log(foundUser.password )
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+})
+
+app.listen(20000,function(){
+    console.log("Server has started on port ghanta")
+})
